794a9a76d55054e247506d153c3bd030bdf59d7e
Minin2707 <nesta.r@yandex.ru>
2025-03-13 00:50:05 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("John", "Doe", (byte) 27);
+        userService.saveUser("Cicada", "7A35090F", (byte) 42);
+        userService.saveUser("Elliot", "Alderson", (byte) 38);
+        userService.saveUser("Josef", "Engressia", (byte) 58);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String userName = "root";
+    private static final String password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/mybase";
+
+
+
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, userName, password);
+    }
+//      в коде везде используется try with resources, вызывать этот метод явно не требуется нигде
+//    public static void close (Connection conn) {
+//        if (conn != null) {
+//            try{
+//                conn.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+
+
+
+
 }
